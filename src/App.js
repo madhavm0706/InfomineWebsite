7a9b8769eafe048769a6e9c97f352d3ff0412be1
Madhav Maheshwari <madhavm0706@gmail.com>
2020-06-13 00:27:54 +0530

    updated all the files and changed tab bar icon

--- src/App.js
+++ src/App.js
@@ -1,24 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+import image from './images/infomine.jpg';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+
+      <img width="300px" height="300px" src={image} style={{margin:"200px"}} />
+      
     </div>
   );
 }
