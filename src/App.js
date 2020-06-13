e23ff02eac3273fb2f17247f9e378b5b5341145b
Madhav Maheshwari <madhavm0706@gmail.com>
2020-06-13 12:52:05 +0530

    Added all pages with routing

--- src/App.js
+++ src/App.js
@@ -3,12 +3,34 @@ import './App.css';
 
 import image from './images/infomine.jpg';
 
+import Home from './pages/Home';
+import Other from './pages/Other';
+import ContactUs from './pages/ContactUs';
+import Error from './pages/Error';
+import Coperate from './pages/Coperate';
+import Economics from './pages/Economics';
+import GandSTax from './pages/GandSTax';
+import IncomeTax from './pages/IncomeTax';
+import IndianC from './pages/IndianC';
+
+import {Switch, Route} from 'react-router-dom';
+
 function App() {
   return (
-    <div>
-
-      <h1>hello from home page</h1>      
-    </div>
+    <>
+     <Switch>
+       <Route exact path="/" component={Home} />
+       <Route exact path="/other" component={Other} />
+       <Route exact path="/contact-us" component={ContactUs} />
+       <Route exact path="/coperate-law" component={Coperate} />
+       <Route exact path="/economics-and-policy" component={Economics} />
+       <Route exact path="/goods-and-service-tax" component={GandSTax} />
+       <Route exact path="/Income-tax" component={IncomeTax} />
+       <Route exact path="/Indian-constitution" component={IndianC} />
+       <Route component={Error} />
+     </Switch>
+            
+    </>
   );
 }
 
---
+++ src/pages/Other.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Other() {
+    return (
+        <div>
+            hello from Other page
+        </div>
+    )
+}
