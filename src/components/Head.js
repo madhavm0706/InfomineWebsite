3923b64e9bd0e08051365af8f980c852c4147f30
SanjanaMaheshwari <sanjana.maheshwari456@gmail.com>
2020-06-15 14:09:30 +0530

    Add Head component

---
+++ src/components/Head.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+const Head = () =>{
+    return(
+        <div className="container-fluid">
+					<div className="container">
+						<div className="row">
+							<div className="col-12 col-md-12 header">
+								
+								<h1 className="logo"><a href="./index.html">InfoMine</a></h1>
+								<p className="tagline">Your Learning Destination</p>
+							</div>
+						</div>
+					</div>
+				</div>
+    )
+}
+
+export default Head;
