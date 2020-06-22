9b41a1f92795e2f4ee8f98d90264f41ef4707ec4
SanjanaMaheshwari <sanjana.maheshwari456@gmail.com>
2020-06-22 13:00:48 +0530

    Add login functionality

--- src/firebase/Firebase.js
+++ src/firebase/Firebase.js
@@ -4,15 +4,25 @@ import 'firebase/auth';
 import 'firebase/storage';
 
 
+// var firebaseConfig = {
+//     apiKey: "AIzaSyAs0qlAayTqp7_eGVcZ2dsOml37GeMygb4",
+//     authDomain: "infomine-364d1.firebaseapp.com",
+//     databaseURL: "https://infomine-364d1.firebaseio.com",
+//     projectId: "infomine-364d1",
+//     storageBucket: "infomine-364d1.appspot.com",
+//     messagingSenderId: "953430068820",
+//     appId: "1:953430068820:web:ec2a991918c5a45aa867c1",
+//     measurementId: "G-V0BBGJP2MV"
+//   };
+
 var firebaseConfig = {
-    apiKey: "AIzaSyAs0qlAayTqp7_eGVcZ2dsOml37GeMygb4",
-    authDomain: "infomine-364d1.firebaseapp.com",
-    databaseURL: "https://infomine-364d1.firebaseio.com",
-    projectId: "infomine-364d1",
-    storageBucket: "infomine-364d1.appspot.com",
-    messagingSenderId: "953430068820",
-    appId: "1:953430068820:web:ec2a991918c5a45aa867c1",
-    measurementId: "G-V0BBGJP2MV"
+    apiKey: "AIzaSyCmbGzoqb95PSr0DQ8uL6mjHJ835Xf0MjU",
+    authDomain: "infomine-basic.firebaseapp.com",
+    databaseURL: "https://infomine-basic.firebaseio.com",
+    projectId: "infomine-basic",
+    storageBucket: "infomine-basic.appspot.com",
+    messagingSenderId: "449759910377",
+    appId: "1:449759910377:web:b55fd5334f8f3513539378"
   };
 
 
@@ -90,6 +100,22 @@ var firebaseConfig = {
 
         return postsArray;
     }
+
+    // login for registered user
+    async login(email, password){
+        const user = await firebase.auth().signInWithEmailAndPassword(email, password).catch(err =>{
+            console.log(err);
+        });
+        return user;
+    }      
+
+    async logout(){
+        const logout = await firebase.auth().signOut().catch(err =>{
+            console.log(err);
+            return err; 
+        });
+        return logout;
+    } 
 
   }
 
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,26 +1,63 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Coloredline from '../components/Colerdline';
 
+import { Redirect} from "react-router-dom";
+import firebase from "../firebase/Firebase";
+import { Auth } from "../context/authContext";
+
 export default function Login() {
+
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [routeRedirect, setRouteRedirect] = useState(false);
+
+    const {state, dispatch} = React.useContext(Auth);
+
+    const login = async(e) => {
+        e.preventDefault();
+        let response = await firebase.login(email, password);
+        if(response.hasOwnProperty("message")){
+            console.log(response.message);
+        }else{
+            //console.log(response.user);
+            setRouteRedirect(true);
+            return dispatch({
+                type: "LOGIN",
+                payload: response.user
+            });
+           
+        }
+    }
+
+
+
+    
+
+    const redirect = routeRedirect;
+    if(redirect){
+        return <Redirect to="/postyourarticle/article" />  
+    }
     return (
         <div className="container">
+            <form onSubmit={login}>
             <div className="form-group">
                 <br></br>
             <h3 align="center">Login To Post Your Article</h3><Coloredline color="black" /> <br />
+            
 
-            <div className="row">
+                <div className="row">
 
 
 
                     <div className="offset-md-5 col-md-4">
                         <label>Username:-</label>
-                        <input type="text" name="articlename" placeholder="Enter Username" required />
+                        <input type="email" name="articlename" placeholder="Enter Username"  onChange={(e) => setEmail(e.target.value)} required />
                     </div>
                 </div>
                 <div className="row">
                     <div className="offset-md-5 col-md-4">
                         <label>Password:-</label>
-                        <input type="password" name="password" placeholder="Enter Your Password " required />
+                        <input type="password" name="password" placeholder="Enter Your Password " onChange={(e) => setPassword(e.target.value)} required />
                     </div>
                 </div>
             </div>
@@ -28,9 +65,14 @@ export default function Login() {
                     <div class="offset-md-5 col-md-4 ">
                     <input type="submit" value="Login" className="btn btn-primary" />
                     </div>&nbsp;
+                
+                    </div>
+
+            </form>
+            
                     
 
-                </div>
+                
             <Coloredline color="black" />
            
         </div>
