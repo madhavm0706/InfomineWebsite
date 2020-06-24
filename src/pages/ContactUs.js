2a52e07eae608a394bb6190c5e8e2953a2910c59
SanjanaMaheshwari <sanjana.maheshwari456@gmail.com>
2020-06-24 16:57:06 +0530

    Add reviews feature (firestore enabled)

--- src/pages/ContactUs.js
+++ src/pages/ContactUs.js
@@ -1,9 +1,60 @@
-import React from 'react'
+import React from 'react';
+//import firebase from "../firebase/Firebase";
+import * as firebase from 'firebase';
+import 'firebase/firestore';
 
-export default function ContactUs() {
+class ContactUs extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+         email: "",
+         review: ""
+        };
+      }
+
+      updateInput = e => {
+        this.setState({
+          [e.target.name]: e.target.value
+        });
+      }
+
+      
+      addUser = e => {
+        e.preventDefault();
+        const db = firebase.firestore();
+        db.settings({
+          timestampsInSnapshots: true
+        });
+        const userRef = db.collection("contactFormDetails").add({
+          email: this.state.email,
+          review: this.state.review
+        });  
+        this.setState({
+            email: "",
+            review: ""
+        });
+      };
+  render() {
     return (
-        <div>
-            hello from ContactUs page
-        </div>
-    )
-}
+        <form onSubmit={this.addUser}>
+          
+          <input
+            type="email"
+            name="email"
+            placeholder="abc@gmail.com"
+            onChange={this.updateInput}
+            value={this.state.email}
+          />
+          <input
+            type="text"
+            name="review"
+            placeholder="review"
+            onChange={this.updateInput}
+            value={this.state.review}
+          />
+          <button type="submit">Submit</button>
+        </form>
+        );
+      }
+   }
+export default ContactUs;
