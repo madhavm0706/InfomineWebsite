fb701193b0d87f127bc53aa082d385a63f8b2fa9
SanjanaMaheshwari <sanjana.maheshwari456@gmail.com>
2020-06-29 17:40:24 +0530

    Store contact details to firestore

--- src/pages/ContactUs.js
+++ src/pages/ContactUs.js
@@ -1,6 +1,47 @@
-import React from 'react'
+import React from 'react';
+//import firebase from "../firebase/Firebase";
+import * as firebase from 'firebase';
+import 'firebase/firestore';
 
-export default function ContactUs() {
+class ContactUs extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+         email: "",
+         message: "",
+         name: "",
+         number: ""
+        };
+      }
+
+      updateInput = e => {
+        this.setState({
+          [e.target.name]: e.target.value
+        });
+      }
+
+      
+      addUser = e => {
+        e.preventDefault();
+        const db = firebase.firestore();
+        db.settings({
+          timestampsInSnapshots: true
+        });
+        const userRef = db.collection("contactFormDetails").add({
+          email: this.state.email,
+          message: this.state.message,
+          name: this.state.name,
+          number: this.state.number
+          
+        });  
+        this.setState({
+          email: "",
+          message: "",
+          name: "",
+          number: ""
+        });
+      };
+  render() {
     return (
         <div className="container">
             <h3 style={{textAlign:"center"}}>Contact US</h3><br /><br />
@@ -18,32 +59,82 @@ export default function ContactUs() {
                <p>We try to serve you the best..!! Happy reading..!!</p>
                <p>You may reach us by filling below information</p>
 
-               <form>
+               <form onSubmit={this.addUser}>
                 <div className="col-md-4">
                     <label htmlFor="name">Name</label>
-                    <input type="text" name="name" placeholder="enter your name" required />
+                    <input 
+                    type="text" 
+                    name="name" 
+                    placeholder="enter your name" 
+                    onChange={this.updateInput}
+                    value={this.state.name}
+                    required />
                         
                     </div>  
                     <div className="col-md-4">
                     <label htmlFor="email">Email</label>
-                    <input type="email" name="email" placeholder="enter E-mail id" required />
+                    <input 
+                    type="email" 
+                    name="email" 
+                    placeholder="enter E-mail id" 
+                    onChange={this.updateInput}
+                    value={this.state.email}
+                    required />
                         
                     </div>
                     <div className="col-md-4">
-                    <label htmlFor="contact">Contact Number</label>
-                    <input type="number" name="contact" placeholder="enter your contact number" required />
+                    <label htmlFor="number">Contact Number</label>
+                    <input 
+                    type="text" 
+                    name="number" 
+                    placeholder="enter your contact number" 
+                    onChange={this.updateInput}
+                    value={this.state.number}
+                     />
+                    
                         
                     </div>
                     <div className="col-md-4">
                     <label htmlFor="message">Message</label>
-                    <textarea name="message" rows="5" placeholder="Type Your Message" ></textarea>
+                    <textarea 
+                    name="message" rows="5" 
+                    placeholder="Type Your Message"
+                    onChange={this.updateInput}
+                    value={this.state.message}
+                     ></textarea>
                         
                     </div> 
                     <div className="col-md-4">
-                        <input type="submit" value="Submit" className="btn btn-primary"></input>
+                        <input 
+                        type="submit" 
+                        value="Submit" 
+                        className="btn btn-primary"></input>
                     </div>
                 </form>    
             
         </div>
     )
 }
+        // <form onSubmit={this.addUser}>
+          
+        //   <input
+        //     type="email"
+        //     name="email"
+        //     placeholder="abc@gmail.com"
+        //     onChange={this.updateInput}
+        //     value={this.state.email}
+        //   />
+        //   <input
+        //     type="text"
+        //     name="review"
+        //     placeholder="review"
+        //     onChange={this.updateInput}
+        //     value={this.state.review}
+        //   />
+        //   <button type="submit">Submit</button>
+        // </form>
+        // );
+      // }
+}
+
+export default ContactUs;
