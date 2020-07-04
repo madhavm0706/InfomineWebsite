a3e838bea6850aedbb0a3911f6a92bf62728b53d
SanjanaMaheshwari <sanjana.maheshwari456@gmail.com>
2020-07-04 17:59:22 +0530

    Add error page

--- src/pages/Error.js
+++ src/pages/Error.js
@@ -1,9 +1,20 @@
 import React from 'react'
+import { MdError } from "react-icons/md";
 
 export default function Error() {
     return (
         <>
-            hello from error page
+        <div className="error-size">
+            <br/>
+        <div className="jumbotron">
+                <h1><MdError /> Oops! You seem to have enter a wrong URL <MdError/></h1>
+                
+        </div>
+        {/* <h3>Re-enter the URL or Contact-us for any help.</h3> */}
+        </div>
+        
+        
+            
         </>
     )
 }
