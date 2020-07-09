2ea8aee2b76b432c03c9f6c35c946cab85d6050c
SanjanaMaheshwari <sanjana.maheshwari456@gmail.com>
2020-07-09 19:38:39 +0530

    Styling of article page

--- src/components/Articledats.js
+++ src/components/Articledats.js
@@ -8,22 +8,26 @@ export default function Articledats({articleinfo}) {
     const {name,publishedBy, date,month,slug,description,image} = articleinfo;
     return (
         <> 
-        
-             <div class="col-md-3 col-sm-6 col-xs-6">
+            
+            <div class="col-lg-4 col-md-6 col-sm-6 col-xs-12">
                 <Link to={`/single-article-page/${slug}`} >
                 <div class="card card-item snip1527">
                      <div class="image">
                       <img src={image} alt="pr-sample23" />
                      </div>
-                <figcaption>
-                   <div class="date"><span class="day">{date}<br></br><p>{month}</p></span></div>
+                <figcaption className="no-expand">
+                    
+                    <div class="date"><span class="day">{date}<br></br><p>{month}</p></span></div>
                     <h4>{name}</h4>
-                    <i class="fas fa-user"> &nbsp;{publishedBy}</i>         
+                    <i class="fas fa-user"> &nbsp;{publishedBy}</i>
+                         
                 </figcaption>
 
                 </div>
                 </Link>
              </div>
+            
+             
              
     </>
     )
