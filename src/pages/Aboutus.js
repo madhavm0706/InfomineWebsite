77c6714352ea775ed6646dd5c5f60c9f8c7d4f6d
madhavm0706 <madhavm0706@gmail.com>
2020-07-16 03:17:26 +0530

    Add About us Page

--- src/pages/Aboutus.js
+++ src/pages/Aboutus.js
@@ -2,8 +2,30 @@ import React from 'react'
 
 export default function Aboutus() {
     return (
-        <div>
-            hello from 
+        <div className="container">
+
+            <div className="aboutushead col-12">
+            
+                   
+                   
+                    <h2 align="center">Who Are We?</h2>
+                
+                   </div>
+               
+
+            
+
+            <div className="aboutuscontent offset-1 col-10 offset-1" >
+                <p>
+                InfoMine is an initiative taken by a group of Indian Chartered Accountants to provide a one stop reading and learning platform to persons from various domains including professionals, business persons, general enthusiastic readers and others. We write articles on various topics viz., Direct and Indirect Taxation, Corporate Laws, Economic Policies, Current Affairs including those relating to International Relations, Polity and Governance, Science and Technology etc. We also provide readers with latest updates, notifications etc., as and when they arrive and an analysis in form of article as soon as possible.
+                </p>
+                <p>
+                We believe that knowledge has wings which know no boundaries and thus, at times, readers may not agree with opinions or analysis provided by us. However, we shall always appreciate reader’s view point and we expect this platform to grow as an interactive one where readers can discuss their queries.
+
+ 
+                </p>
+            </div>
+          
         </div>
     )
 }
